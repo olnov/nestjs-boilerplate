cd711ffaee861db76bae8c9644d9b9c8ad8530fe
Oleg Novikov <o.novikov@ymail.com>
2025-09-14 00:00:14 +0100

    fix(dotenv): added @nestjs/config package and configured globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,19 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from 'pino-nestjs';
 import pino from 'pino';
 import { ExampleModule } from './example/example.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ExampleModule,
     LoggerModule.forRoot({
       pinoHttp: {
-        stream: pino.destination ({
+        stream: pino.destination({
           sync: false,
         }),
       },
     }),
+    ConfigModule.forRoot(),
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from 'pino-nestjs';
 
@@ -9,9 +12,9 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
     { bufferLogs: true },
-  )
+  );
 
-  console.log(`NODE_ENV ${process.env.NODE_ENV}`)
+  console.log(`NODE_ENV ${process.env.NODE_ENV}`);
 
   const config = new DocumentBuilder()
     .setTitle('MyApp')
@@ -19,10 +22,10 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
-  const documentFactory = ()=> SwaggerModule.createDocument(app, config);
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1/docs', app, documentFactory);
 
-  if (process.env.NODE_ENV==='production') {
+  if (process.env.NODE_ENV === 'production') {
     app.useLogger(app.get(Logger));
   }
 
